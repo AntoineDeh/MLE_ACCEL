e78dd18fa2c866eb813be636dade6c456c0c2e45
Antoine Dehoux <antoine.dehoux@reseau.eseo.fr>
2023-12-22 19:01:01 +0100

    MAE

--- projetMLE.py
+++ projetMLE.py
@@ -1,58 +1,29 @@
-'''
-ouvrir un fichier
-plot
-filtrer
-plot
-labeliser-pre traitement
-on enregistre dans datasets avec les filtres
-
-création de l'entrainement
-
-build model
-save model h5 et json
-
-evaluate plot
-
-
-README
-
-'''
-
+import tkinter as tk
+from tkinter import filedialog, messagebox, simpledialog
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-
-# Importation des modules spécifiques de TensorFlow
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import SGD
-
-import pandas as pd
 from scipy.signal import butter, filtfilt, freqz
-import matplotlib.pyplot as plt
 from scipy.fft import fft
-import numpy as np
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
-# Importation des modules spécifiques de TensorFlow
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import SGD
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-
+# Initialisation des variables globales
+canvas = None
+plot_frame = None
+model = None
+X_train, X_test, y_train, y_test = None, None, None, None
+y_train_encoded, y_test_encoded = None, None
+history = None
+data = None
+root = None
 
 def init():
     # Initialisation de la graine aléatoire pour la reproductibilité
@@ -60,9 +31,9 @@ def init():
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
-
 def load_csv_file():
     """ Charge un fichier CSV et retourne les données. """
+    global root
     file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     if file_path:
         try:
@@ -75,6 +46,7 @@ def load_csv_file():
 
 def filter_data(data):
     """ Demande à l'utilisateur s'il souhaite filtrer les données et les filtre si nécessaire. """
+    global root
     t_min = simpledialog.askinteger("Time Min", "Enter minimum time (T min in ms):", parent=root)
     t_max = simpledialog.askinteger("Time Max", "Enter maximum time (T max in ms):", parent=root)
     if t_min is not None and t_max is not None:
@@ -89,6 +61,7 @@ def apply_filter(data, fs, fc, order):
 
 def plot_data(data):
     """ Effectue des visualisations des données. """
+    global canvas, plot_frame
     if data.empty:
         messagebox.showinfo("Info", "No data to plot.")
         return
@@ -110,27 +83,25 @@ def plot_data(data):
     ax.legend()
     ax.grid(True)
 
-    global canvas
     if canvas is not None:
         canvas.get_tk_widget().destroy()
     canvas = FigureCanvasTkAgg(fig, master=plot_frame)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-
-
-def build_model(input_shape1, input_shape2):
-   # Construction du modèle
+def build_model():
+    """ Construction du modèle """
+    global model, X_train
     model = Sequential([
-    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    Dense(128, activation='relu'),
-    Flatten(),
-    Dense(1, activation='sigmoid')
+        Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+        Dense(128, activation='relu'),
+        Flatten(),
+        Dense(1, activation='sigmoid')
     ])
-
-    save_model(model)
+    return model
 
 def save_model(model):
+    """ Enregistre le modèle entraîné """
     file_path = filedialog.asksaveasfilename(title="Enregistrer le modèle", defaultextension=".h5",
                                              filetypes=[("H5 Files", "*.h5")])
     if file_path:
@@ -140,24 +111,29 @@ def save_model(model):
         except Exception as e:
             messagebox.showerror("Erreur", f"Erreur lors de l'enregistrement du modèle: {e}")
 
-def train_test():
-    # Séparation en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+def train_test_split_data():
+    """ Sépare les données en ensembles d'entraînement et de test """
+    global X_train, X_test, y_train, y_test, data
+    # Ici, vous devez définir x et y en fonction de vos données
+    # Exemple : x = data.drop('label_column', axis=1), y = data['label_column']
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-#model.summary()
-
-def compile():
-    # Compilation du modèle
+def compile_model():
+    """ Compilation du modèle """
+    global model
     opt = SGD(learning_rate=0.001, momentum=0.2)
     model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
-def train():
-    # Entraînement avec validation et arrêt anticipé
+def train_model():
+    """ Entraînement du modèle """
+    global model, X_train, y_train, history
     callback = EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
-    history = model.fit(X_train, y_train_encoded, validation_split=0.15, epochs=500, batch_size=30, callbacks=[callback], use_multiprocessing=True)
+    # Ici, assurez-vous que y_train est encodé si nécessaire
+    history = model.fit(X_train, y_train, validation_split=0.15, epochs=500, batch_size=30, callbacks=[callback], use_multiprocessing=True)
 
 def print_accuracy():
-    # Visualisation de la perte et de la précision
+    """ Visualisation de la perte et de la précision """
+    global history
     plt.figure()
     plt.plot(history.history['loss'], color='teal', label='loss')
     plt.plot(history.history['val_loss'], color='orange', label='val_loss')
@@ -171,55 +147,69 @@ def print_accuracy():
     plt.legend()
     plt.show()
 
-def evaluation():
-    # Évaluation du modèle
-    scores = model.evaluate(X_test, y_test_encoded)
+def evaluate_model():
+    """ Évaluation du modèle """
+    global model, X_test, y_test
+    scores = model.evaluate(X_test, y_test)
     print("\nÉvaluation sur le test data %s: %.2f - %s: %.2f%% " % (model.metrics_names[0], scores[0], model.metrics_names[1], scores[1] * 100))
 
+def main_menu():
+    global plot_frame, root
+    root = tk.Tk()
+    root.title("Data Analysis and Model Training")
 
-def analyse_frequentiel(donnees):
-    tau = 0.01
-    Te = tau / 1000
-    Fe = 1 / Te
-    NFFT = 2**20
-    f = np.arange(NFFT // 2 + 1) / NFFT
-    F = f * Fe
+    plot_frame = tk.Frame(root)
+    plot_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-    cutoff_frequency = 0.25  # Exemple de fréquence de coupure, à ajuster selon les besoins
-    order = 1  # Ordre du filtre Butterworth
+    def load_and_plot():
+        global data
+        data = load_csv_file()
+        if data is not None:
+            plot_data(data)
 
-    num, den = butter(order, 2 * cutoff_frequency)
-    H_PB = freqz(num, den, worN=f, fs=1)
+    def filter_and_plot():
+        global data
+        data = filter_data(data)
+        plot_data(data)
 
-    # Affichage dans le domaine fréquentiel
-    signal_x = donnees['AccX [mg]']
-    signal_y = donnees['AccY [mg]']
-    signal_z = donnees['AccZ [mg]']
-    fft_result_x = fft(signal_x)
-    fft_result_y= fft(signal_y)
-    fft_result_z = fft(signal_z)
-    freq = np.fft.fftfreq(len(signal_x), d=(donnees['T [ms]'][1] - donnees['T [ms]'][0]) / 1000.0)
+    def build_save_and_train_model():
+        global model
+        build_model()
+        save_model(model)
+        train_test_split_data()
+        compile_model()
+        train_model()
+        print_accuracy()
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(freq, np.abs(fft_result_x), label='AccX [mg]')
-    plt.plot(freq, np.abs(fft_result_z), label='AccY [mg]')
-    plt.plot(freq, np.abs(fft_result_z), label='AccZ [mg]')
-    plt.xlabel('Fréquence (Hz)')
-    plt.ylabel('Amplitude')
-    plt.title('Domaine fréquentiel des données de l\'accéléromètre (AccX , AccYet AccZ)')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    def evaluate():
+        evaluate_model()
 
+    # Menu buttons
+    tk.Button(root, text='Load CSV and Plot', command=load_and_plot).pack(fill=tk.X)
+    tk.Button(root, text='Filter Data and Replot', command=filter_and_plot).pack(fill=tk.X)
+    tk.Button(root, text='Build, Save and Train Model', command=build_save_and_train_model).pack(fill=tk.X)
+    tk.Button(root, text='Evaluate Model', command=evaluate).pack(fill=tk.X)
 
+    root.mainloop()
 
 if __name__ == "__main__":
     init()
+    main_menu()
+
+
+
+'''
+
+if __name__ == "__main__":
+   init()
     load_csv_file()
     
+   #Pour tous les fichiers de Datas faire :
     filter_data()
     apply_filter()
     plot_data()
+    analyse_frequentiel() // avoir un bouton suivant ensuite pour passer au suivant // chacun de ces fichiers vont dans le dossier output
+    ############""puis 
 
     build_model()
     save_model()
@@ -227,8 +217,10 @@ if __name__ == "__main__":
     train_test()
     compile()
 
+
     train()
     print_accuracy()
-    evaluation()
-    analyse_temporelle()
-    
+  ///Autant de fois que l'on veut on peut faire : 
+    evaluation() // un bouton suivant apparait après pour passer à un autre ou faire exit
+
+'''
